f3c23e3c17e47d1fd7a6af961bc1393cc84ae294
Faizan. A Rakin <rakinfaizanahmad@gmail.com>
2025-01-31 14:44:45 -0800

    Added results that show total expenxe and balance
    Defined inputs as floats and created print statements that show total expenses and remaining balance

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 #Asking Monthly allowance
-monthly_allowance = input("What is your monthly allowance? : $")
+monthly_allowance = float(input("What is your monthly allowance? : $"))
 
 # Expenses
-expense_1 = input("How much do you pay monthly for rent? : $")
-expense_2 = input("How much do you spend on groceries per month : $")
-expense_3 = input("How much do you spend on transportation per month : $")
-expense_4 = input("How much do you spend on entertainment per month : $")
+expense_1 = float(input("How much do you pay monthly for rent? : $"))
+expense_2 = float(input("How much do you spend on groceries per month : $"))
+expense_3 = float(input("How much do you spend on transportation per month : $"))
+expense_4 = float(input("How much do you spend on entertainment per month : $"))
 
-total_expenses = expense_1+expense_2+expense_3+expense_4
+total_expenses = expense_1 + expense_2 + expense_3 + expense_4
+balance = monthly_allowance - total_expenses
 
 # Results 
-print(total_expenses)
+print("Your monthly allowance is: $",monthly_allowance)
 
+print("Total expenses: $",round(total_expenses, 2))
+
+print("Your remaining balance: $", balance)
