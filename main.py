ccafd0c6442c6d53ce9eeb24969bd4189bb61c49
Faizan. A Rakin <rakinfaizanahmad@gmail.com>
2025-01-31 14:26:08 -0800

    Added input variables for monthly allowance and expenses
    One variable for monthly expenses and 4 monthly expenses variable created in main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+#Asking Monthly allowance
+monthly_allowance = input("What is your monthly allowance? : $")
+
+# Expenses
+expense_1 = input("How much do you pay monthly for rent? : $")
+expense_2 = input("How much do you spend on groceries per month : $")
+expense_3 = input("How much do you spend on transportation per month : $")
+expense_4 = input("How much do you spend on entertainment per month : $")
+
+total_expenses = expense_1+expense_2+expense_3+expense_4
+
+# Results 
+print(total_expenses)
+
