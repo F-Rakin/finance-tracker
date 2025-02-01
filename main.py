86eb2f300e62f300250a9a52b2f9e13813f2ba02
F-Rakin <rakinfaizanahmad@gmail.com>
2025-01-31 23:20:18 -0800

    Added conditional statements and functions
    Prints conditional message based on spendings. Everything is under functions now and the main function calls the whole program

--- main.py
+++ main.py
@@ -1,18 +1,53 @@
-#Asking Monthly allowance
-monthly_allowance = float(input("What is your monthly allowance? : $"))
+# Expenses (Float numbers)
 
-# Expenses
-expense_1 = float(input("How much do you pay monthly for rent? : $"))
-expense_2 = float(input("How much do you spend on groceries per month : $"))
-expense_3 = float(input("How much do you spend on transportation per month : $"))
-expense_4 = float(input("How much do you spend on entertainment per month : $"))
+def expenses():
+    """
+    Asks user for different expenses in a month
 
-total_expenses = expense_1 + expense_2 + expense_3 + expense_4
-balance = monthly_allowance - total_expenses
+    :return: Stores Sum(expenses)
+    """
+
+    rent = float(input("How much do you pay monthly for rent? : $"))
+    groceries = float(input("How much do you spend on groceries per month : $"))
+    transportation = float(input("How much do you spend on transportation per month : $"))
+    entertainment = float(input("How much do you spend on entertainment per month : $"))
+
+    total_expenses = rent + groceries + transportation + entertainment   # Adds up all the expenses
+    return total_expenses
 
 # Results 
-print("Your monthly allowance is: $",monthly_allowance)
 
-print("Total expenses: $",round(total_expenses, 2))
+def results(monthly_allowance, total_expenses):
+    """
+    Prints results based on inputted values
+    
+    :return: Allowance, Total expense and Balance
+    """
+    balance = monthly_allowance - total_expenses  # Calculates remaining balance
 
-print("Your remaining balance: $", balance)
+    print("Your monthly allowance is: $",monthly_allowance)
+    print("Total expenses: $",round(total_expenses, 2))
+    print("Your remaining balance: $",round(balance, 2))
+
+    return balance
+
+# Conditional statements depending on spendings
+
+def conditions(balance):
+    """
+    Gives a conditional statement based on spendings
+    
+    :return: A text message
+    """
+    if balance > 0:
+        print("Congrats! You have more money to spend.")  # Will show this message if you underspend
+    else: 
+        print("You have spent too much!!! Plan your spendings ") # Will show this message if you overspend
+
+def main():
+    monthly_allowance = float(input("What is your monthly allowance? : $"))
+    calculate_expenses = expenses()
+    show_remaining_balance = results(monthly_allowance, calculate_expenses)
+    conditions(show_remaining_balance)
+
+main()
